9dd4113e467150bf20750bda12e3fb4c7ad74c6b
Prajit Dhara <114762489+pr4jitd@users.noreply.github.com>
2024-11-10 09:41:06 -0500

    Removing the Google scholar citations to clean up page

--- _cite/cite.py
+++ _cite/cite.py
@@ -28,7 +28,7 @@ log("Compiling sources")
 sources = []
 
 # in-order list of plugins to run
-plugins = ["orcid", "google-scholar", "sources"]
+plugins = ["orcid",  "sources"] #"google-scholar"
 
 # loop through plugins
 for plugin in plugins:
